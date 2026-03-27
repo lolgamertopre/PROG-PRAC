6ec71e478e275374bb54bac65dff2200341b542d
lolgamertopre <kasherwinbanquil@gmail.com>
2026-03-27 18:10:39 +0800

    sheesh

---
+++ void no return basic parameter.c
@@ -0,0 +1,21 @@
+//Write the displayMessage function here
+void displayMessage(){
+printf ("Welcome to the C Functions Lab!");
+}
+
+//Write the printNumber function here
+void printNumber (int n){
+printf ("%d", n);
+}
+
+//Write the getOneHundred function here
+int getOneHundered (){
+   return 100;
+}
+
+
+//Write the addNumbers function here
+int addNumbers ( int a, int b){
+    return a + b;
+}
+
